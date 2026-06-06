4aff3690f4ad4ebb9d102ff3900c7de235b2d851
Lochie <95149116+Lochi-dot-JPEG@users.noreply.github.com>
2026-06-06 23:29:24 +1000

    feat: player movement

--- src/main.c
+++ src/main.c
@@ -1,54 +1,68 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
+#include "raymath.h"
+#include "resource_dir.h" // utility header for SearchAndSetResourceDir
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+int main() {
+  // Tell the window to use vsync and work on high DPI displays
+  SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-int main ()
-{
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+  int windowW = 800;
+  int windowH = 600;
+  // Create the window and OpenGL context
+  InitWindow(windowW, windowH, "Hello Raylib");
 
-	// Create the window and OpenGL context
-	InitWindow(800, 600, "Hello Raylib");
+  // Utility function from resource_dir.h to find the resources folder and set
+  // it as the current working directory so we can load from it
+  SearchAndSetResourceDir("resources");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+  Texture wabbit = LoadTexture("wabbit_alpha.png");
+  Vector2 wabbitSize = {wabbit.width, wabbit.height};
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
-	// game loop
-	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
-	{
-		// drawing
-		BeginDrawing();
+  float wabbitX = 0;
+  float wabbitY = 0;
+  int wabbitSpeed = 500;
 
-		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
+  int currentFps = 60;
+  SetTargetFPS(60);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+  while (!WindowShouldClose()) {
+    float mouseWheel = GetMouseWheelMove();
+    if (mouseWheel != 0) {
+      currentFps += (int)mouseWheel;
+      if (currentFps < 0)
+        currentFps = 0;
+      SetTargetFPS(currentFps);
+    }
+    // drawing
+    BeginDrawing();
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
-		
-		// end the frame and get ready for the next one  (display frame, poll input, etc...)
-		EndDrawing();
-	}
+    // Setup the back buffer for drawing (clear color and depth buffers)
+    ClearBackground(BLACK);
 
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+    // draw some text using the default font
+    DrawText("Hello Raylib", 200, 200, 20, WHITE);
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+    if (IsKeyDown(KEY_D)) {
+      wabbitX += wabbitSpeed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_A)) {
+      wabbitX -= wabbitSpeed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_W)) {
+      wabbitY -= wabbitSpeed * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_S)) {
+      wabbitY += wabbitSpeed * GetFrameTime();
+    }
+    wabbitX = Clamp(wabbitX, 0, windowW - wabbitSize.x);
+    wabbitY = Clamp(wabbitY, 0, windowH - wabbitSize.y);
+
+    DrawTexture(wabbit, (int)wabbitX, (int)wabbitY, WHITE);
+
+    EndDrawing();
+  }
+
+  UnloadTexture(wabbit);
+  CloseWindow();
+  return 0;
 }
